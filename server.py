f433f438b16900454996d5bf032781c873f526f8
chrisdavis <chrisdperumal@gmail.com>
2024-02-12 17:18:56 -0500

    Create server.py

---
+++ server.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+#make calls to this server from chrome extension
+
+@app.route("/")
+def hello_world():
+    hello_world_string = "<p>Welcome to News Analyzer</p>"
+    return(hello_world_string)
